9cb1246b0da542ac5a72dd5e6b4678e6e5685808
Harsh Tripathi <harshravi324@gmail.com>
2024-04-28 03:16:23 +0530

    custom auth user model created

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
