35fa75f6e405d851750cc2a8f44c7b24a944be66
Harsh Tripathi <harshravi324@gmail.com>
2024-04-28 20:03:20 +0530

    updated method added

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -32,3 +32,8 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
